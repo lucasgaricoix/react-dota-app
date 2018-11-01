87120d66a6599fb22fce2b8281c534756085446f
lucasgaricoix <lucasgaricoix@hotmail.com>
2018-10-31 23:37:23 -0300

    Updates

---
+++ precache-manifest.d70c2b3124ae1648baf56902e846a23b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "29b6856df2df717f7ce4db2f3458e747",
+    "url": "/react-dota-app/static/media/dota2-background.29b6856d.png"
+  },
+  {
+    "revision": "977aded85deccdc48e71",
+    "url": "/react-dota-app/static/js/runtime~main.977aded8.js"
+  },
+  {
+    "revision": "3b20747d9ff34fd1e12b",
+    "url": "/react-dota-app/static/js/main.3b20747d.chunk.js"
+  },
+  {
+    "revision": "33ec9e760c2ef3a83c59",
+    "url": "/react-dota-app/static/js/1.33ec9e76.chunk.js"
+  },
+  {
+    "revision": "3b20747d9ff34fd1e12b",
+    "url": "/react-dota-app/static/css/main.7ca49eed.chunk.css"
+  },
+  {
+    "revision": "8ccbf476e3d98dd10872a6c465069fde",
+    "url": "/react-dota-app/index.html"
+  }
+];
